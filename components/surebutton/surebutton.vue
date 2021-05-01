40b3f0125bda82c9d9291a446fcee55061443f1d
suzhiqiu <zhiqiu.su@joymo.tech>
2021-05-01 08:29:38 +0800

    组件父子传递数据

--- components/surebutton/surebutton.vue
+++ components/surebutton/surebutton.vue
@@ -4,6 +4,9 @@
 	</view>
 </template>
 
+
+
+
 <script>
 	export default {
 		beforeCreate() {
@@ -27,8 +30,9 @@
 		},
 		methods: {
 			onClick() {
-				console.log('被点击了');
-				this.$emit('change','_xx_')
+				console.log('点击了');
+				//子组件向父组件传递数据的方式 触发方式
+				this.$emit('click','_xx_1')
 			}
 		}
 	}
@@ -45,3 +49,18 @@
 	
 
 </style>
+
+
+<!-- 加冒号的，说明后面的是一个变量或者表达式；没加冒号的后面就是对应的字符串字面量！
+
+加上冒号会首先将这个属性当作变量解析，没加冒号就直接是字符串
+
+列：
+
+  <input :disable="true">打开</input>
+
+输出：true 或 false   值是变量/Boolean类型
+
+<input disable="true">打开</input>
+
+输出：true   值是String类型 -->
--- pages/cart/cart.vue
+++ pages/cart/cart.vue
@@ -1,6 +1,9 @@
 <template>
 	<view>
 		<text>购物车页面</text>
+		<button :data-index="12" @click1="clickNextPage(1,$event)">点击1</button>
+		<button @click="clickNextPage(2,$event)">点击2</button>
+		<surebutton textColor ="red" @click="clickCommit">我爱罗</surebutton>
 	</view>
 </template>
 
@@ -8,11 +11,19 @@
 	export default {
 		data() {
 			return {
-				
+
 			}
 		},
 		methods: {
-			
+			//事件传值:
+			clickNextPage(index,event) {
+				console.log(event.currentTarget.dataset.index);
+				console.log(index,event);
+			},
+			clickCommit(text){
+				console.log('提交',text);
+			},
+
 		}
 	}
 </script>
