5d34609ee138170ec90486c9dedc9782838c1e12
suzhiqiu <zhiqiu.su@joymo.tech>
2021-05-27 17:37:23 +0800

    高度的处理

---
+++ pages/tabbar/layout/custom-text/custom-text.vue
@@ -0,0 +1,20 @@
+<template>
+	<text>1123</text>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.sub {
+		width: 100%;
+		height: 100%;
+	}
+</style>
--- pages/tabbar/layout/layout.vue
+++ pages/tabbar/layout/layout.vue
@@ -2,12 +2,24 @@
 	<view class="container">
 		<text class="view1">view1_{{getLogin1}}</text>
 		<text class="view2">view2_{{getLogin2()}}</text>
-		<button @click="clickLogin">点击1</button>
+		<view class="view3">
+			<text class="view4">我是子组件1</text>
+			<text class="view5">我是子组件2</text>
+			<view class="view6">我是子组件3</view>
+			<text class="view7">我是子组件4</text>
+		</view>
+		<button @click="clickLogin">点击3</button>
+		
+	
 	</view>
 </template>
 
 <script>
+	import customtText from './custom-text/custom-text.vue'
 	export default {
+		components:{
+			customtText,
+		},
 		data() {
 			return {
 				login: false,
@@ -63,6 +75,17 @@
 			color: yellow;
 			font-size: 28rpx;
 			font-weight: 400rpx;
+		}
+		
+		.view3 {
+			background-color: red;
+		
+		}
+		.view4 {
+			background-color: blue;
+		}
+		.view5 {
+			background-color: yellow;
 		}
 
 	}
